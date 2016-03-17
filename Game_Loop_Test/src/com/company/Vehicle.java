9408d35e5e506c063a35230a28dd7832ac5ae007
Jonas Kloster <joklost@gmail.com>
2016-03-17 09:44:02 +0100

    Magnus' game loop example

---
+++ Game_Loop_Test/src/com/company/Vehicle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by Magnus on 16-03-2016.
+ */
+public class Vehicle {
+    public int PosX = 0;
+    public int PosY = 0;
+    public int Velocity = 0;
+    public int VectorX = 0;
+    public int VectorY = 0;
+    public int Size = 1;
+
+    public char Model;
+}
