83d8dc59a5f4a2d4e3f88b424933447671b31e30
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-03-31 18:38:04 +0200

    Opdaterer scanner og token kode

--- battlesim-scanner/src/com/company/Token.java
+++ battlesim-scanner/src/com/company/Token.java
@@ -17,6 +17,7 @@ public class Token {
     public Token(int type, String value) {
         this.type = type;
         this.value = value;
+
     }
 
     public int getType() {
@@ -28,7 +29,7 @@ public class Token {
     }
 
     public String toString() {
-        return Token.getTokenName(type) + (value == "" ? "" : ": " + value);
+        return (value == "" ? Token.getTokenName(type) : value);
     }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -37,59 +38,95 @@ public class Token {
 ////////////////////////////////////////////////////////////////////////////////
 
     public final static int
-            /*  Keywords */
-            BOOLEAN             = 0,  IF                  = 1,
-            WHILE               = 2,  DO                  = 3,
-            FOREACH             = 4,  FUNCTION            = 5,
-            END                 = 6,  BEGIN               = 7,
-            PROGRAM             = 8,  FOR                 = 9,
-            TYPE                = 10, AS                  = 11,
-            DECIMAL             = 12, STRING              = 13,
-            DECLARE             = 14, IN                  = 15,
-            RETURN              = 16, ELSE                = 17,
-            LIST                = 18, INCLUDE             = 19,
-            DEFINE              = 20, SWITCH              = 21,
-            NULL_LITERAL        = 22, CASE                = 23,
-            DEFAULT             = 24,
+        /*  Keywords */
+        BOOLEAN             = 0,
+        IF                  = 1,
+        WHILE               = 2,
+        DO                  = 3,
+        FOREACH             = 4,
+        FUNCTION            = 5,
+        END                 = 6,
+        BEGIN               = 7,
+        PROGRAM             = 8,
+        FOR                 = 9,
+        TYPE                = 10,
+        AS                  = 11,
+        DECIMAL             = 12,
+        STRING              = 13,
+        DECLARE             = 14,
+        IN                  = 15,
+        RETURN              = 16,
+        ELSE                = 17,
+        LIST                = 18,
+        //INCLUDE             = 19,
+        //DEFINE              = 20,
+        SWITCH              = 21,
+        NULL_LITERAL        = 22,
+        CASE                = 23,
+        DEFAULT             = 24,
 
-    /* Seperators */
-            BOOLEAN_LITERAL     = 25, LPAREN              = 26,
-            RPAREN              = 27, LBRACE              = 28,
-            RBRACE              = 29, COMMA               = 30,
-            DOT                 = 31,
+        BOOLEAN_LITERAL     = 25,
 
-    /* Operators */
-            EQ                  = 32, PLUSPLUS            = 33,
-            MINUSMINUS          = 34, PLUS                = 35,
-            MINUS               = 36, DIV                 = 37,
-            MULT                = 38, MOD                 = 39,
-            PLUSEQ              = 40, MINUSEQ             = 41,
-            MULTEQ              = 42, DIVEQ               = 43,
-            MODEQ               = 44, AND                 = 45,
-            OR                  = 46, NOT                 = 47,
-            EQUALS              = 48, GREATERTHAN         = 49,
-            GREATERTHANEQUALS   = 50, LESSTHAN            = 51,
-            LESSTHANEQUALS      = 52,
+        /* Seperators */
+        LPAREN              = 26,
+        RPAREN              = 27,
+        LBRACE              = 28,
+        RBRACE              = 29,
+        COMMA               = 30,
+        DOT                 = 31,
 
-    /* Literals */
-            INTEGER_LITERAL     = 53, DECIMAL_LITERAL     = 54,
-            STRING_LITERAL      = 55,
+        /* Operators */
+        EQ                  = 32,
+        PLUSPLUS            = 33,
+        MINUSMINUS          = 34,
+        PLUS                = 35,
+        MINUS               = 36,
+        DIV                 = 37,
+        MULT                = 38,
+        MOD                 = 39,
+        PLUSEQ              = 40,
+        MINUSEQ             = 41,
+        MULTEQ              = 42,
+        DIVEQ               = 43,
+        MODEQ               = 44,
+        AND                 = 45,
+        OR                  = 46,
+        NOT                 = 47,
+        EQUALS              = 48,
+        GREATERTHAN         = 49,
+        GREATERTHANEQUALS   = 50,
+        LESSTHAN            = 51,
+        LESSTHANEQUALS      = 52,
 
-    /* Other */
-            IDENTIFIER          = 56, EOF                 = 57,
-            EOL                 = 58,
+        /* Literals */
+        INTEGER_LITERAL     = 53,
+        DECIMAL_LITERAL     = 54,
+        STRING_LITERAL      = 55,
 
-    /* For iterators */
-            DOWNTO              = 59, TO                  = 60,
+        /* Other */
+        IDENTIFIER          = 56,
+        EOF                 = 57,
+        EOL                 = 58,
 
-    /* Extras (dem vi glemte) */
-            THEN                = 61, VOID                = 62,
-            OF                  = 63,
+        /* For iterators */
+        DOWNTO              = 59,
+        TO                  = 60,
 
-    /* Domain Specific */
-            TERRAIN             = 64, GROUP               = 65,
-            PLATOON             = 66, FORCE               = 67,
-            COORD               = 68, SOLDIER             = 69;
+        /* Extras (dem vi glemte) */
+        THEN                = 61,
+        VOID                = 62,
+        OF                  = 63,
+
+        /* Domain Specific */
+        TERRAIN             = 64,
+        GROUP               = 65,
+        PLATOON             = 66,
+        FORCE               = 67,
+        COORD               = 68,
+        SOLDIER             = 69,
+        SIMULATION          = 70,
+        STEP                = 71,
+        INTERRUPT           = 72;
 
     private static HashMap<Integer, String> tokenNames;
     static {
@@ -115,8 +152,8 @@ public class Token {
         tokenNames.put(RETURN, "RETURN");
         tokenNames.put(ELSE, "ELSE");
         tokenNames.put(LIST, "LIST");
-        tokenNames.put(INCLUDE, "INCLUDE");
-        tokenNames.put(DEFINE, "DEFINE");
+        //tokenNames.put(INCLUDE, "INCLUDE");
+        //tokenNames.put(DEFINE, "DEFINE");
         tokenNames.put(SWITCH, "SWITCH");
         tokenNames.put(NULL_LITERAL, "NULL_LITERAL");
         tokenNames.put(CASE, "CASE");
@@ -176,6 +213,9 @@ public class Token {
         tokenNames.put(FORCE, "FORCE");
         tokenNames.put(COORD, "COORD");
         tokenNames.put(SOLDIER, "SOLDIER");
+        tokenNames.put(SIMULATION, "SIMULATION");
+        tokenNames.put(STEP, "STEP");
+        tokenNames.put(INTERRUPT, "INTERRUPT");
     }
 
     public static String getTokenName(int token) {
