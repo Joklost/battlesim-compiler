79497e822a10a87774a5e3e9d5a1d950309e5477
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-04-01 20:35:32 +0200

    nu compiler det

--- battlesim-CUPparser/src/com/company/Main.java
+++ battlesim-CUPparser/src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new java.io.FileReader("/home/joklost/git/P4-Code/Code_Examples/BattleSim_CodeExamples/BubbleSort.bs"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Parser parser = new Parser(scanner);
+        try {
+            parser.parse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
