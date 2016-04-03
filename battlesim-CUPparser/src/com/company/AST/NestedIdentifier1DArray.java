5623abf42c5cccb02dee304142eec2aa83e9d571
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-04-03 16:18:48 +0200

    Tilføjer 2d nested id

--- battlesim-CUPparser/src/com/company/AST/NestedIdentifier1DArray.java
+++ battlesim-CUPparser/src/com/company/AST/NestedIdentifier1DArray.java
@@ -3,10 +3,10 @@ package com.company.AST;
 /**
  * Created by joklost on 02-04-16.
  */
-public class NestedIdentifierArray extends NestedIdentifier {
+public class NestedIdentifier1DArray extends NestedIdentifier {
     public Expression expression;
 
-    public NestedIdentifierArray(Identifier id, Expression e, int ln) {
+    public NestedIdentifier1DArray(Identifier id, Expression e, int ln) {
         super(id, ln);
         this.expression = e;
     }
--- battlesim-CUPparser/src/com/company/AST/NestedIdentifier1DArrayMember.java
+++ battlesim-CUPparser/src/com/company/AST/NestedIdentifier1DArrayMember.java
@@ -3,11 +3,11 @@ package com.company.AST;
 /**
  * Created by joklost on 01-04-16.
  */
-public class NestedIdentifierArrayMember extends NestedIdentifier {
+public class NestedIdentifier1DArrayMember extends NestedIdentifier {
     public Expression expression;
     public NestedIdentifier nestedIdentifier;
 
-    public NestedIdentifierArrayMember(Identifier id, Expression e, NestedIdentifier nid, int ln) {
+    public NestedIdentifier1DArrayMember(Identifier id, Expression e, NestedIdentifier nid, int ln) {
         super(id, ln);
         this.expression = e;
         this.nestedIdentifier = nid;
