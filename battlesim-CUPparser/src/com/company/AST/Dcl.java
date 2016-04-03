a908d0ab4e6915fcee6fe11584e37e99cab3a830
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-04-03 14:16:21 +0200

    Ændringer parametre til node

--- battlesim-CUPparser/src/com/company/AST/Dcl.java
+++ battlesim-CUPparser/src/com/company/AST/Dcl.java
@@ -4,14 +4,12 @@ package com.company.AST;
  * Created by joklost on 01-04-16.
  */
 public class Dcl extends Stmt {
-    public String identifier;
-    //public DclIdList dclIdList;
+    public DclIdList dclIdList;
     public TypeIdentifier typeIdentifier;
 
-    public Dcl(String id, /*DclIdList d, */TypeIdentifier t, int ln) {
+    public Dcl(DclIdList d, TypeIdentifier t, int ln) {
         super(ln);
-        this.identifier = id;
-        //this.dclIdList = d;
+        this.dclIdList = d;
         this.typeIdentifier = t;
     }
 
