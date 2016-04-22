2b9f85a65243a31268a7f2d14d12cf50f568b03f
Monzy <Magnushansen12@hotmail.com>
2016-04-22 15:50:59 +0200

    Tilføjer dagens arbjede på DST

---
+++ battlesim-compiler/src/com/company/DST.java
@@ -0,0 +1,169 @@
+package com.company;
+
+import com.sun.deploy.services.PlatformType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Magnus on 22-04-2016.
+ */
+class DSTFunctions{
+    public static Vector FindUnitVector(Coord startCoord, Coord targetCoord){
+        Vector res = new Vector();
+        res.X = targetCoord.X - startCoord.X;
+        res.Y = targetCoord.Y - startCoord.Y;
+        double length = res.GetLength();
+        res.X /= length;
+        res.Y /= length;
+        return res;
+    }
+
+    public static Coord CenterOfMass(List<Coord> coords){
+        Coord res = new Coord();        
+        for(Coord coord : coords){
+            res.X += coord.X;
+            res.Y += coord.Y;
+        }
+        res.X /= coords.size();
+        res.Y /= coords.size();
+        
+        return res;
+    }
+}
+
+class Coord{
+    public double X = 0;
+    public double Y = 0;
+
+    public Coord(){
+
+    }
+}
+
+class Vector{
+    public double X = 0;
+    public double Y = 0;
+
+    public Vector(){
+
+    }
+
+    public double GetLength(){
+        return Math.abs(Math.sqrt( Math.pow(X, X) + Math.pow(Y, Y)));
+    }
+}
+
+class Barrier{
+    public List<Coord> Verticies;
+
+    public void AddVerticy(Coord vertex){
+        this.Verticies.add(vertex);
+    }
+}
+
+class Map{
+    public int Width = 0;
+    public int Height = 0;
+}
+
+class Soldier {
+    private double DefaultVelocity = 3; //Change this because this is a random number
+    private int Size = 4;            //Change this because this is a random number
+
+    public double Velocity = 0;
+    public Vector Direction;
+    public Coord SCoord;
+    public int Magazines = 4;
+
+    public void Move(Coord target){
+        if(Velocity == 0){
+            Velocity = DefaultVelocity;
+        }
+        Direction = DSTFunctions.FindUnitVector(SCoord, target);
+    }
+
+    public void Move(Coord target, double velocity){
+        Velocity = velocity;
+        Direction = DSTFunctions.FindUnitVector(SCoord, target);
+    }
+
+    public void StopMovement(){
+        Velocity = 0;
+    }
+
+}
+
+class Group{
+    public List<Soldier> Soldiers;
+
+    public void AddSoldiers(Soldier ... soldiers){
+        for(Soldier s : soldiers)
+            this.Soldiers.add(s);
+    }
+
+    public void Move(Coord target, double velocity){
+        SetVelocity(velocity);
+        SetDirection(DSTFunctions.FindUnitVector(DSTFunctions.CenterOfMass(this.GetCoordList()), target));
+    }
+
+    public void SetVelocity(double velocity){
+        for(Soldier s : Soldiers)
+            s.Velocity = velocity;
+    }
+
+    public void SetDirection(Vector direction){
+        for(Soldier s : Soldiers){
+            s.Direction = direction;
+        }
+    }
+
+    public List<Coord> GetCoordList(){
+        List<Coord> res = new ArrayList<>();
+        for(Soldier s : Soldiers){
+            res.add(s.SCoord);
+        }
+        return res;
+    }
+}
+
+class Platoon{
+    public List<Group> Groups;
+
+    public void AddGroups(Group ... groups){
+        for(Group g : groups)
+            this.Groups.add(g);
+    }
+
+    public void AddSoldiers(Soldier ... soldiers){
+        Group nGroup = new Group();
+        for(Soldier s: soldiers)
+            nGroup.AddSoldiers(s);
+        AddGroups(nGroup);
+    }
+}
+
+class Force{
+    public List<Platoon> Platoons;
+
+    public void AddPlatoons(Platoon ... platoons){
+        for(Platoon p : platoons)
+            this.Platoons.add(p);
+    }
+
+    public void AddGroups(Group ... groups){
+        Platoon nPlat = new Platoon();
+
+        for(Group g : groups)
+            nPlat.AddGroups(g);
+        AddPlatoons(nPlat);
+    }
+
+    public void AddSoldier(Soldier ... soldiers){
+        Group nGroup = new Group();
+
+        for(Soldier s : soldiers)
+            nGroup.AddSoldiers(s);
+        AddGroups(nGroup);
+    }
+}
