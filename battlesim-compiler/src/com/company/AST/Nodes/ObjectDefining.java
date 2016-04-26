f74343eabf8e7edc75f41d1fd1ffaa3c78623331
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-04-26 12:53:46 +0200

    #28 - ændrer navn på type node

--- battlesim-compiler/src/com/company/AST/Nodes/ObjectDefining.java
+++ battlesim-compiler/src/com/company/AST/Nodes/ObjectDefining.java
@@ -3,9 +3,9 @@ package com.company.AST.Nodes;
 /**
  * Created by joklost on 4/26/16.
  */
-public class CustomTypeIdentifier extends TypeName {
+public class ObjectDefining extends TypeName {
     public Identifier name;
-    public CustomTypeIdentifier(Identifier id, int ln) {
+    public ObjectDefining(Identifier id, int ln) {
         super(ln);
         name = id;
     }
