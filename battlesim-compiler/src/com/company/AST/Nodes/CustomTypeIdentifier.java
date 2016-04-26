c79594e164a940c1317bc3c20a8e4c37cf7fb9e6
Jonas Kloster Jacobsen <joklost@gmail.com>
2016-04-26 12:47:45 +0200

    #28 - ændrer start noden, og tilføjer ny til custom type

--- battlesim-compiler/src/com/company/AST/Nodes/CustomTypeIdentifier.java
+++ battlesim-compiler/src/com/company/AST/Nodes/CustomTypeIdentifier.java
@@ -3,7 +3,7 @@ package com.company.AST.Nodes;
 /**
  * Created by joklost on 4/26/16.
  */
-public class CustomTypeIdentifier extends TypeIdentifier {
+public class CustomTypeIdentifier extends TypeName {
     public Identifier name;
     public CustomTypeIdentifier(Identifier id, int ln) {
         super(ln);
--- battlesim-compiler/src/com/company/AST/Nodes/Start.java
+++ battlesim-compiler/src/com/company/AST/Nodes/Start.java
@@ -10,10 +10,11 @@ public class Start extends ASTNode {
     public FunctionDclList functionDclList1;
     public Program program;
 
-    public Start(DclBlock dcls, SimBlock sb, FunctionDclList fd1, Program p, int ln) {
+    public Start(DclBlock dcls, SimBlock sb, TypeDeclarationList tl, FunctionDclList fd1, Program p, int ln) {
         super(ln);
         this.dclBlock = dcls;
         this.simBlock = sb;
+        this.typeDeclarationList = tl;
         this.functionDclList1 = fd1;
         this.program = p;
     }
