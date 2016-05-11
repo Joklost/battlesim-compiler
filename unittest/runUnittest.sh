10d8ebce421d44d774bed220e8c51604faabbed5
Jesper Nielsen <jesper@windelborg.dk>
2016-05-11 15:53:57 +0200

    Fjerner unused var

--- unittest/runUnittest.sh
+++ unittest/runUnittest.sh
@@ -7,7 +7,6 @@ PRINTERROR=false
 function assert {
 	# $1 compile filename
 	# $2 expected result filename
-	# $3
 	if [ $PRINTERROR = true ]; then
 		java -jar $path_to_compiler	$1
 		java -jar Main.jar > results_real
@@ -35,7 +34,6 @@ function assert {
 function assertCompileError {
 	# $1 compile filename
 	# $2 expected result filename
-	# $3 test number
 	if [ $PRINTERROR = true ]; then
 		java -jar $path_to_compiler	$1 2> results_real
 	else
@@ -55,16 +53,16 @@ function assertCompileError {
 	fi
 }
 
-assert Test1_BubbleSort.bs Test1_results_expt 1 #!! Sorta broken/useless after d9f9320
-assert Test2_Recursion.bs Test2_results_expt 2
-assert Test3_ReturnInteger.bs Test3_results_expt 3
-assert Test4_ReturnDecimal.bs Test4_results_expt 4
-assert Test5_TestWhile.bs Test5_results_expt 5
-assert Test6_TestArrayForeach.bs Test6_results_expt 6
-assert Test7_TestMath.bs Test7_results_expt 7
-assert Test8_TestBool.bs Test8_results_expt 8
-assert Test9_TestIf.bs Test9_results_expt 9
-assert Test10_TestSwitch.bs Test10_results_expt 10
-assert Test11_TestFor.bs Test11_results_expt 11
-assert Test12_TestScope.bs Test12_results_expt 12
-assert Test13_CustomTypeParam.bs Test13_results_expt 13
+assert Test1_BubbleSort.bs Test1_results_expt #!! Sorta broken/useless after d9f9320
+assert Test2_Recursion.bs Test2_results_expt
+assert Test3_ReturnInteger.bs Test3_results_expt
+assert Test4_ReturnDecimal.bs Test4_results_expt
+assert Test5_TestWhile.bs Test5_results_expt
+assert Test6_TestArrayForeach.bs Test6_results_expt
+assert Test7_TestMath.bs Test7_results_expt
+assert Test8_TestBool.bs Test8_results_expt
+assert Test9_TestIf.bs Test9_results_expt
+assert Test10_TestSwitch.bs Test10_results_expt
+assert Test11_TestFor.bs Test11_results_expt
+assert Test12_TestScope.bs Test12_results_expt
+assert Test13_CustomTypeParam.bs Test13_results_expt
