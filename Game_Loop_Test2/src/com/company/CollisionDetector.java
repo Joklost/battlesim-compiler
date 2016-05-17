3a812fd26c68f7b23195d3cb73575ddbc94b91d0
Monzy <Magnushansen12@hotmail.com>
2016-05-17 10:10:31 +0200

    flytter på projectileCollistion til CollisionDetector

--- Game_Loop_Test2/src/com/company/CollisionDetector.java
+++ Game_Loop_Test2/src/com/company/CollisionDetector.java
@@ -1,22 +1,30 @@
 package com.company;
 
+import com.company.Objects.Bullet;
 import com.company.Objects.SimulationObjects.*;
+import com.company.Objects.StaticObjects.Coord;
 import com.company.Objects.StaticObjects.Vector;
+
+import java.util.List;
 
 /**
  * Created by Magnus on 03-05-2016.
  */
 public class CollisionDetector {
 
-    public static void visualDetect(Force allies, Force enemies){
+    public static void detectAllCollisions(Force allies, Force enemies, List<Bullet> bullets){
         for(Platoon p: allies.Platoons){
             for(Group g: p.Groups){
                 for(Soldier s: g.Soldiers){
                     for(Platoon p2: enemies.Platoons){
                         for(Group g2: p2.Groups){
                             for(Soldier s2: g2.Soldiers){
-                                detectEnemy(s, s2);
-                                detectEnemy(s2, s);
+                                for(Bullet b: bullets){
+                                    detectProjectileCollisions(s, b);
+                                    detectProjectileCollisions(s2, b);
+                                }
+                                tryDetectEnemy(s, s2);
+                                tryDetectEnemy(s2, s);
                             }
                         }
                     }
@@ -25,7 +33,7 @@ public class CollisionDetector {
         }
     }
 
-    private static void detectEnemy(Soldier s, Soldier enemy){
+    private static void tryDetectEnemy(Soldier s, Soldier enemy){
         if(!s.canStillSeeEnemy()){
             if(!s.IsDead() && !enemy.IsDead()){
                 Vector v = Vector.getVectorByPoints(s.getPos(), enemy.getPos());
@@ -35,6 +43,19 @@ public class CollisionDetector {
                 else{
                     s.isEnemyDetected = false;
                 }
+            }
+        }
+    }
+
+    private static void detectProjectileCollisions(Soldier soldier, Bullet bullet){
+        if(bullet.owner != soldier.side){
+            Vector bulToSol = Vector.getVectorByPoints(bullet.firePos, soldier.getPos());
+            Vector projection = bullet.vector.normalize();
+            double projLength = bulToSol.dot(bullet.vector.normalize());
+            projection.scale(projLength);
+            Vector dist = Vector.getVectorByPoints(new Coord(bullet.firePos.x + projection.x, bullet.firePos.y + projection.y), soldier.getPos());
+            if(dist.getLength() < soldier.size){
+                soldier.kill();
             }
         }
     }
